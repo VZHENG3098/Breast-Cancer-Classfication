9d63a231e9fb88cef0641cd422813cab8740bdbe
VZHENG3098 <vzheng3098@bths.edu>
2021-12-03 21:52:16 -0500

    Fixed Decision Tree
    - Remade Breast CSV to fix error

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import csv
+import pandas
+
+
 
 with open('breast-cancer.data', 'r') as input_file:
     lines = input_file.readlines()
@@ -6,7 +9,7 @@ with open('breast-cancer.data', 'r') as input_file:
     for line in lines:
         newLine = line.strip().split()
         newLines.append(newLine)
-
+print(newLines)
 labels = ["Class", "age", "menopause", "tumor-size", "inv-nodes", "node-caps", "deg-malig", "breast", "breast-quad",
           "irradiat"]
 with open('Breast.csv', 'w', newline="") as test_file:
@@ -15,6 +18,13 @@ with open('Breast.csv', 'w', newline="") as test_file:
     for line in newLines:
         lineSplit = line[0].split(",")
         lineAdd = []
-        for index in range(0, len(lineSplit) - 1):
+        for index in range(0, len(lineSplit)):
             lineAdd.append(lineSplit[index].replace("-", "--"))
         file_writer.writerow(lineAdd)
+
+# Convert CSV File and replacing first column to last
+df = pandas.read_csv('Breast.csv')
+print(df.keys())
+
+df = df[["age", "menopause", "tumor-size", "inv-nodes", "node-caps", "deg-malig", "breast", "breast-quad","irradiat","Class"]]
+df.to_csv('Breast.csv',index=False)
